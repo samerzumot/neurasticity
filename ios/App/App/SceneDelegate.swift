967ca66d3e742d1e76a9a51d28d0eac3a10e9368
Samer Zumot <54731842+samerzumot@users.noreply.github.com>
2026-08-25 10:24:05 -0400

    fix(simulator): remove invalid macos entitlements from ios target and fix scene delegate window loading

--- ios/App/App/SceneDelegate.swift
+++ ios/App/App/SceneDelegate.swift
@@ -5,20 +5,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-
-        #if targetEnvironment(macCatalyst)
-        if let titlebar = windowScene.titlebar {
-            titlebar.titleVisibility = .visible
-            titlebar.toolbar = nil
-        }
-        windowScene.sizeRestrictions?.minimumSize = CGSize(width: 1024, height: 720)
-        #endif
-
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = CAPBridgeViewController()
-        window?.makeKeyAndVisible()
-
+        guard let _ = scene as? UIWindowScene else { return }
         SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
     }
 
