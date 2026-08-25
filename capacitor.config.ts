b3e0e08e26c1de31062f214c5a0cbb0ae347f787
Samer Zumot <54731842+samerzumot@users.noreply.github.com>
2026-08-25 11:27:11 -0400

    chore(ios): update Xcode project bundle identifier to com.brainswell.app and sync plist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.brainwell.app',
+  appId: 'com.brainswell.app',
   appName: 'Brainswell',
   webDir: 'dist'
 };
